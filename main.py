9c23d05757953baa296b8e2a7c42fd0647bc2009
wiltrahan <twilorip@gmail.com>
2017-02-07 19:23:24 -0500

    developed front page, set up db

--- main.py
+++ main.py
@@ -15,11 +15,55 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    post = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainPage(Handler):
+
+    def render_front(self, title="", post="", error=""):
+        posts = db.GqlQuery("SELECT * FROM Post "
+                            "ORDER BY created DESC ")
+        self.render("front.html", title=title, post=post, error=error, posts=posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        post = self.request.get("post")
+
+        if title and post:
+            p = Post(title = title, post = post)
+            p.put()
+
+            self.redirect("/")
+        else:
+            error = "You must enter a title, and a post!"
+            self.render_front(title, post, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
